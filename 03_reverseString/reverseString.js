e9535ca869e473761ad9406f79789c89decde236
BaptisteLenain <baptiste.lenain@hotmail.fr>
2022-08-20 16:41:55 +0200

    reverseString done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(words) {
+    let myArray = words.split("");
+    let myLetter = '';
 
+    for (i = 0; i < myArray.length; i++) {
+        myLetter = myArray.shift();
+        myArray.splice(myArray.length - i, 0, myLetter);
+        myLetter = '';
+    }
+    console.log(myArray);
+    words = myArray.join('');
+    return words;
 };
 
 // Do not edit below this line
