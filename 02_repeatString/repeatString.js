90af0af7e86378371ea06b029aa8545aa7635473
BaptisteLenain <baptiste.lenain@hotmail.fr>
2022-08-20 15:50:34 +0200

    repeatString done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+let addString = '';
 
+const repeatString = function(string, num) {
+    addString = ''
+    if (num < 0) {
+        return 'ERROR';
+    } else { 
+        for (i = 1; i <= num; i++) {
+        addString += string;
+       // console.log(addString);
+        }
+    return addString;
+    }
 };
 
 // Do not edit below this line
