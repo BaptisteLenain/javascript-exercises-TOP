03f39b8d4e494e2b38869bed8f041be477770cc9
BaptisteLenain <baptiste.lenain@hotmail.fr>
2022-08-20 18:35:54 +0200

    removeFromArray done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(baseArray, ...theArgs) {
+    console.log(baseArray);
+    console.log(theArgs);
+    baseArray = baseArray.filter((item) => !theArgs.includes(item));
+    return baseArray;
+    console.log(baseArray);
 };
 
 // Do not edit below this line
