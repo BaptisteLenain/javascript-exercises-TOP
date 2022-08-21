2e7cd6e8c160d5541053e072cf9a65310c110c92
BaptisteLenain <baptiste.lenain@hotmail.fr>
2022-08-21 11:48:43 +0200

    sumAll done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(start,end) {
+    let total = 0;
+    if (start < 0 || end < 0 || !Number.isInteger(start) || !Number.isInteger(end)) {
+        return "ERROR";
+    } else {
+        for (i = Math.min(start,end); i <= Math.max(start,end); i++) {
+            total = total + i;
+        }
+    return total;
+    }
 };
 
 // Do not edit below this line
