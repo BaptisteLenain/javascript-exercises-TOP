cc42f883888bbff435a7527d26e6d47788dd6495
BaptisteLenain <baptiste.lenain@hotmail.fr>
2022-08-21 12:34:55 +0200

    tempConversion done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temperatureF) {
+  let temperatureC = Math.round(((temperatureF - 32) * 5/9) * 10) / 10;
+  console.log(temperatureC);
+  return temperatureC;
 };
 
-const ctof = function() {
-
+const ctof = function(temperatureC) {
+  let temperatureF = Math.round((temperatureC * 1.8 + 32) * 10) / 10;
+  console.log(temperatureF);
+  return temperatureF;
 };
 
 // Do not edit below this line
